dc8652b1deefd2caebdb60f8b70e713a6c3bb82a
timkeniatim <121697645+timkeniatim@users.noreply.github.com>
2025-10-03 18:28:02 +0200

    hahah

--- src/Neuron.java
+++ src/Neuron.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Neuron {
+    int aktFkt;
+    ArrayList<Double> weights;
+    public Neuron(int aktFkt){
+        this.aktFkt = aktFkt;
+    }
+
+    public double outputFkt(ArrayList<Double> input){
+        for(int i = 0; i <= input.size(); i++){
+            weights.add(Math.random());
+        }
+        double sum = 0;
+        for(int j = 0; j< input.size();j++){
+            sum+= weights.get(j)* weights.get(j);
+        }
+        return sum;
+    }
 }
